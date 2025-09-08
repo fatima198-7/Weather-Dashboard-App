9823eebe3476f2748eede364dca0c3e18ad7df2a
fatima198-7 <fatima198ch@gmail.com>
2025-09-08 09:47:04 +0500

    removed hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </HashRouter>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
