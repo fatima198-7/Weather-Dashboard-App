53b259c35400fa8282c7edc447f7c8cc47d3541d
fatima198-7 <fatima198ch@gmail.com>
2025-09-08 09:43:38 +0500

    deployed

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 );
